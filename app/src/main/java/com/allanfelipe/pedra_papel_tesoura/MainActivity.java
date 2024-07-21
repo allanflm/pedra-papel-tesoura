581c39aa229ae8e211198f3ba5d6b97f0156190a
Allan <allanfelipedk@gmail.com>
2024-07-21 16:55:15 -0300

    feat: generating random choice

--- app/src/main/java/com/allanfelipe/pedra_papel_tesoura/MainActivity.java
+++ app/src/main/java/com/allanfelipe/pedra_papel_tesoura/MainActivity.java
@@ -2,10 +2,17 @@ package com.allanfelipe.pedra_papel_tesoura;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    private final String TESOURA = "TESOURA";
+    private final String PEDRA = "PEDRA";
+    private final String PAPEL = "PAPEL";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,18 +21,43 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void selecionarPedra(View view) {
-        verificarGanhador("Pedra");
+        verificarGanhador(PEDRA);
     }
 
     public void selecionarPapel(View view) {
-        verificarGanhador("Papel");
+        verificarGanhador(PAPEL);
     }
 
     public void selecionarTesoura(View view) {
-        verificarGanhador("Tesoura");
+        verificarGanhador(TESOURA);
     }
 
-    private void verificarGanhador(String escolhaUsuario) {
-        System.out.println("Item clicado: " + escolhaUsuario);
+    private String gerarEscolhaAleatoriaApp() {
+        String[] opcoes = {PEDRA, PAPEL, TESOURA};
+        int numeroAleatorio = new Random().nextInt(3);
+        String escolhaDoApp = opcoes[numeroAleatorio];
+
+        ImageView imagemApp = findViewById(R.id.image_app);
+
+        switch (escolhaDoApp) {
+            case PEDRA:
+                imagemApp.setImageResource(R.drawable.pedra);
+                break;
+            case PAPEL:
+                imagemApp.setImageResource(R.drawable.papel);
+                break;
+            case TESOURA:
+                imagemApp.setImageResource(R.drawable.tesoura);
+                break;
+        }
+        return escolhaDoApp;
+
     }
+    private void verificarGanhador(String escolhaUsuario) {
+        String escolhaDoApp = gerarEscolhaAleatoriaApp();
+        //System.out.println("Item clicado: " + escolhaUsuario);
+
+    }
+
+
 }
