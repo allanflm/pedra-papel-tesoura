3a41316addb8c62b43bb29e268990e08b713f17b
Allan <allanfelipedk@gmail.com>
2024-07-21 16:41:10 -0300

    feat: Choosing option - Rock Paper or Scissors App

--- app/src/main/java/com/allanfelipe/pedra_papel_tesoura/MainActivity.java
+++ app/src/main/java/com/allanfelipe/pedra_papel_tesoura/MainActivity.java
@@ -1,24 +1,31 @@
 package com.allanfelipe.pedra_papel_tesoura;
 
 import android.os.Bundle;
+import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    }
+
+    public void selecionarPedra(View view) {
+        verificarGanhador("Pedra");
+    }
+
+    public void selecionarPapel(View view) {
+        verificarGanhador("Papel");
+    }
+
+    public void selecionarTesoura(View view) {
+        verificarGanhador("Tesoura");
+    }
+
+    private void verificarGanhador(String escolhaUsuario) {
+        System.out.println("Item clicado: " + escolhaUsuario);
     }
 }
